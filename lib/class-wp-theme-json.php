91e4a32fde6c2be12b81fecf2f2044d5d4702056
André <andres.maneiro@automattic.com>
2021-01-14 13:00:10 +0100

    Prepare code & test for get_settings

--- lib/class-wp-theme-json.php
+++ lib/class-wp-theme-json.php
@@ -551,21 +551,6 @@ class WP_Theme_JSON {
 	}
 
 	/**
-	 * Given a context, it returns its settings subtree.
-	 *
-	 * @param array $context Context adhering to the theme.json schema.
-	 *
-	 * @return array|null The settings subtree.
-	 */
-	private static function extract_settings( $context ) {
-		if ( empty( $context['settings'] ) ) {
-			return null;
-		}
-
-		return $context['settings'];
-	}
-
-	/**
 	 * Given a tree, it creates a flattened one
 	 * by merging the keys and binding the leaf values
 	 * to the new keys.
@@ -975,12 +960,7 @@ class WP_Theme_JSON {
 	 * @return array Settings per context.
 	 */
 	public function get_settings() {
-		return array_filter(
-			array_map( array( $this, 'extract_settings' ), $this->theme_json ),
-			function ( $element ) {
-				return null !== $element;
-			}
-		);
+		return $this->theme_json['settings'];
 	}
 
 	/**
